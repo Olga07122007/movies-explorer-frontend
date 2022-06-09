1847cc2dd63610a3dbf9d786219a6634e56978c4
Olga Yagnyuk <olga07122007@yandex.ru>
2022-06-09 18:54:21 +0300

    Исправлены ошибки верстки

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -6,7 +6,7 @@ function SearchForm() {
       <div className="search__container">
         <div className="search__icon"></div>
         <form className="search__form" name="searchForm" noValidate>
-          <input className="search__input" type="text" name="filmInput" id="filmInput" placeholder="Фильм" required minLength="2" maxLength="100" />
+          <input className="search__input" type="text" name="filmInput" id="filmInput" required placeholder="Фильм" minLength="2" maxLength="100" />
           <button type="submit" className="search__search-btn"></button>
         </form>
         <div className="search__shortFilms">
